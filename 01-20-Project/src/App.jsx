ee07edec746948c73ce18bc59415f86fde67a70b
Prantor-Das <prantor.college@gmail.com>
2025-11-02 10:42:30 +0530

    Initialize React project with Vite, adding components for Accordion, Random Color, and Star Rating

---
+++ 01-20-Project/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import Accordian from './components/01-accordian'
+import RandomColor from './components/02-random-color'
+import StarRating from './components/03-star-rating'
+
+function App() {
+
+  return (
+    <div>
+      <h1 style={{ textAlign: "center"}}>Accordian</h1>
+      <Accordian />
+      <br />
+      <hr />
+      <br />
+      <h1 style={{ textAlign: "center"}}>Random Color</h1>
+      <RandomColor />
+      <br />
+      <hr />
+      <br />
+      <h1 style={{ textAlign: "center"}}>Star Rating</h1>
+      <StarRating noOfStars={10} />
+    </div>
+  )
+}
+
+export default App
