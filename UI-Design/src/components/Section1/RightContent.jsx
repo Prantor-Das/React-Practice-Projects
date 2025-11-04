0f1eb23d90597004443916a54105b6e5776b91d2
Prantor-Das <prantor.college@gmail.com>
2025-11-04 09:41:38 +0530

    Add user data handling and create RightCard components for display

--- UI-Design/src/components/Section1/RightContent.jsx
+++ UI-Design/src/components/Section1/RightContent.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import RightCard from "./RightCard";
 
-const RightContent = () => {
+const RightContent = (props) => {
+  // safe optional chaining to avoid reading .color on undefined
+  console.log(props.users?.color);
+
+  const users = Array.isArray(props.users) ? props.users : [];
+
   return (
-    <div className='h-full w-2/3'>
-        
+    <div
+      id="right"
+      className="h-full flex rounded-4xl overflow-x-auto flex-nowrap gap-10 p-6 w-2/3"
+    >
+      {users.map(function (elem, idx) {
+        return (
+          <RightCard
+            key={idx}
+            color={elem.color}
+            id={idx}
+            img={elem.img}
+            tag={elem.tag}
+          />
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default RightContent
+export default RightContent;
